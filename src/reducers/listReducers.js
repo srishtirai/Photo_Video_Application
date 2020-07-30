199686c7674ce22f7833c2731af6d781f16ee390
srishtirai <srishticrai@gmail.com>
2020-07-30 12:30:09 +0530

    Dummy store data created.

--- src/reducers/listReducers.js
+++ src/reducers/listReducers.js
@@ -4,6 +4,9 @@ import {types} from '../actions/actionTypes';
 const deviceList = (state = {}, action) => {
 	switch (action.type) {
 		case types.GET_LIST_DEVICES: {
+            let deviceListData = action.devices;
+			const newState = {devices: deviceListData, deviceIndex: void 0};
+			return Object.assign({}, state, newState);
         }
     }
 }
--- src/store/store.js
+++ src/store/store.js
@@ -1,1 +1,7 @@
+import {createStore} from 'redux'
+
 import listReducer from '../reducers/listReducers';
+
+const store=createStore(listReducer)
+
+export default store
